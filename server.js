87c1357cac61a5244117908768a9eb03b89069ad
danursin <danursin@gmail.com>
2017-11-18 10:50:46 -0600

    add server js

---
+++ server.js
@@ -0,0 +1,23 @@
+import * as hapi from "hapi";
+import Routes from "./routes";
+
+const localPort = 4321;
+const server = new hapi.Server();
+
+server.connection({ 
+    port : (process.env.PORT || localPort),
+    routes: { cors: true }
+});
+
+server.route(Routes);
+
+server.start( (err) => {
+    if (err) {
+        console.log('error', err.message, err.name, err.stack);
+    } else {
+        console.log('Server running at: '+ server.info.uri);
+    }
+});
+
+
+
